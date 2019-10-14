1fdb0dda1c43a56cdaf87e3684bb79a6e6de19e7
Wiktoria2308 <44002423+Wiktoria2308@users.noreply.github.com>
2019-10-14 21:02:33 +0200

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+package todolist;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Controller todolist = new Controller();
+		
+		todolist.play();
+	}
+
+}
